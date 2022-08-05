48f979d77951efcd271d637665b2cfe34dec99d1
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-05 19:42:36 -0300

    mais requisitos, vambooooooora!

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
 const btnCriaTarefa = document.getElementById('criar-tarefa');
 const getLista = document.getElementById('lista-tarefas');
 
+let selectedItem;
+let count = 0;
+
+function verificaCores() {
+  let getListaItems = document.querySelectorAll('li');
+  for(let index = 0; index < getListaItems.length; index++) {
+    if(getListaItems[index].className === selectedItem){
+      getListaItems[index].style.backgroundColor = 'gray';
+    } else {
+      getListaItems[index].style.backgroundColor = 'white';
+    }
+  }
+}
+
 btnCriaTarefa.addEventListener('click', () => {
   let getInputText = document.getElementById('texto-tarefa').value;
-  let criaTarefa = document.createElement('li');
-  let textContent = document.createTextNode(getInputText);
-  criaTarefa.appendChild(textContent);
-  getLista.appendChild(criaTarefa).lastChild;
-  document.getElementById('texto-tarefa').value = '';
+
+  if(getInputText !== '') {
+    let criaTarefa = document.createElement('li');
+    criaTarefa.className = 'item' + count;
+    count++;
+
+    let textContent = document.createTextNode(getInputText);
+
+    criaTarefa.appendChild(textContent);
+    getLista.appendChild(criaTarefa).lastChild;
+    
+    criaTarefa.addEventListener('click', () => {
+      selectedItem = criaTarefa.className;
+      verificaCores();
+    });
+
+    document.getElementById('texto-tarefa').value = '';
+  }
 });
